efe83cf6118ced2b7dd5376a057aa4f15525dada
hwangdaesun <hds650588@gmail.com>
2023-08-13 17:43:51 +0900

    feat : 회원 계정 엔티티 및 각 계층 구현

---
+++ src/main/java/com/theZ/dotoring/app/memberAccount/model/MemberAccount.java
@@ -0,0 +1,71 @@
+package com.theZ.dotoring.app.memberAccount.model;
+
+
+import com.theZ.dotoring.app.certificate.model.Certificate;
+import com.theZ.dotoring.common.CommonEntity;
+import com.theZ.dotoring.enums.Status;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberAccount extends CommonEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "memberId", nullable = false)
+    private Long id;
+
+    @Column(name = "LOGIN_ID", unique = true)
+    @Size(min = 8, max = 12)
+    private String loginId;
+
+    //    @Size(min = 7, max = 12)
+    private String password;
+
+    @Email
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToMany(mappedBy = "memberAccount")
+    private List<Certificate> certificates = new ArrayList<>();
+
+    @Builder
+    public MemberAccount(String loginId, String password, String email, List<Certificate> certificates) {
+        this.loginId = loginId;
+        this.password = password;
+        this.email = email;
+        this.status = Status.WAIT;
+        this.certificates = certificates;
+    }
+
+    public static MemberAccount createMemberAccount(String loginId, String password, String email, List<Certificate> certificates){
+        MemberAccount memberAccount = MemberAccount.builder()
+                .loginId(loginId)
+                .password(password)
+                .email(email)
+                .certificates(certificates)
+                .build();
+        memberAccount.mappingCertificate(certificates);
+        return memberAccount;
+    }
+
+    public void mappingCertificate(List<Certificate> certificates){
+        for(Certificate certificate : certificates){
+            certificate.mappingMemberAccount(this);
+        }
+    }
+
+}
