4fd6760f5f04de23f1849a3985062f99d91d5c61
hwangdaesun <hds650588@gmail.com>
2023-08-13 23:17:01 +0900

    feat : 아이디 중복 검사 기능

---
+++ src/main/java/com/theZ/dotoring/app/memberAccount/dto/MemberLoginIdRequestDTO.java
@@ -0,0 +1,13 @@
+package com.theZ.dotoring.app.memberAccount.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Pattern;
+
+@Data
+public class MemberLoginIdRequestDTO {
+
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{8,12}$", message = "아이디는 영문과 숫자를 포함한 8~12글자여야 합니다.")
+    private String loginId;
+
+}
---
+++ src/main/java/com/theZ/dotoring/exception/LoginIdDuplicateException.java
@@ -0,0 +1,15 @@
+package com.theZ.dotoring.exception;
+
+import com.theZ.dotoring.common.MessageCode;
+import lombok.Getter;
+
+@Getter
+public class LoginIdDuplicateException extends RuntimeException{
+
+    public final MessageCode messageCode;
+
+    public LoginIdDuplicateException(MessageCode messageCode) {
+        this.messageCode = messageCode;
+    }
+
+}
