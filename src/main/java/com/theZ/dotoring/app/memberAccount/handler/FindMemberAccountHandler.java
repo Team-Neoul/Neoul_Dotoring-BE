8034459a137a82dff3238414bddc43096a9c89bc
hwangdaesun <hds650588@gmail.com>
2023-08-17 13:37:23 +0900

    feat : 아이디 및 비밀번호 찾기 기능 구현(추가)

---
+++ src/main/java/com/theZ/dotoring/app/memberAccount/handler/FindMemberAccountHandler.java
@@ -0,0 +1,40 @@
+package com.theZ.dotoring.app.memberAccount.handler;
+
+import com.theZ.dotoring.app.memberAccount.service.MemberAccountService;
+import com.theZ.dotoring.app.memberAccount.service.MemberEmailService;
+import com.theZ.dotoring.app.mento.dto.EmailCodeRequestDTO;
+import com.theZ.dotoring.app.mento.dto.MemberPasswordRequestDTO;
+import com.theZ.dotoring.common.RedisUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FindMemberAccountHandler {
+
+    private final MemberAccountService memberAccountService;
+    private final MemberEmailService memberEmailService;
+    private final RedisUtil redisUtil;
+
+    public String executeForLoginId(EmailCodeRequestDTO emailCodeRequestDTO){
+        String email = memberEmailService.validateCode(emailCodeRequestDTO.getCode(),emailCodeRequestDTO.getEmail());
+        redisUtil.deleteData(emailCodeRequestDTO.getCode());
+        return memberAccountService.findLoginId(email);
+    }
+
+    public void executeForPassword(MemberPasswordRequestDTO memberPasswordRequestDTO) {
+
+        /**
+         *  로그인 아이디를 사용해, 이메일을 찾는다. 후에 이 이메일이 요청받은 이메일과 일치하는 지 확인한다. -> 오류가 있다면 에러 반환
+         *  일치한다면, 이메일을 사용하여 비밀번호를 발송한 후에 회원의 비밀번호를 변경한다.
+         */
+        memberAccountService.validateLoginIdForEmail(memberPasswordRequestDTO);
+        String password = makePassword();
+        memberEmailService.sendPasswordByEmail(memberPasswordRequestDTO.getEmail(), password);
+        memberAccountService.updatePassword(memberPasswordRequestDTO.getEmail(),password);
+    }
+
+    private String makePassword(){
+        return "dotoring1234";
+    }
+}
