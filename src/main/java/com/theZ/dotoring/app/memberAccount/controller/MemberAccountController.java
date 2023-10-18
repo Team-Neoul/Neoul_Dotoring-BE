18c6847f557b8f7338a5910666e7811886dc4525
Sonny <hds650588@gmail.com>
2023-10-18 14:30:18 +0900

    doc : 각 EndPoint마다 설명 추가

--- src/main/java/com/theZ/dotoring/app/memberAccount/controller/MemberAccountController.java
+++ src/main/java/com/theZ/dotoring/app/memberAccount/controller/MemberAccountController.java
@@ -8,6 +8,7 @@ import com.theZ.dotoring.app.mento.dto.EmailCodeRequestDTO;
 import com.theZ.dotoring.app.mento.dto.MemberPasswordRequestDTO;
 import com.theZ.dotoring.common.ApiResponse;
 import com.theZ.dotoring.common.ApiResponseGenerator;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -24,55 +25,64 @@ public class MemberAccountController {
     private final MemberEmailService memberEmailService;
     private final FindMemberAccountHandler findMemberAccountHandler;
 
+    @ApiOperation(value = "아이디 중복 검사", notes = "아이디는 영문과 숫자를 포함한 8~12글자여야 합니다.")
     @PostMapping("/member/valid-loginId")
     public ApiResponse<ApiResponse.CustomBody<Void>> validateMemberLoginId(@RequestBody @Valid MemberLoginIdRequestDTO memberLoginIdRequestDTO){
         memberAccountService.checkDuplicateAboutLoginId(memberLoginIdRequestDTO.getLoginId());
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
 
-    @PostMapping("/member/valid-code")
-    public ApiResponse<ApiResponse.CustomBody<Void>> validateMemberEmailCode(@RequestBody @Valid MemberEmailCodeRequestDTO memberEmailCodeRequestDTO){
-        memberEmailService.validateCode(memberEmailCodeRequestDTO.getEmailVerificationCode(),memberEmailCodeRequestDTO.getEmail());
-        return ApiResponseGenerator.success(HttpStatus.OK);
-    }
-
-    @PostMapping("/member/signup/valid-code")
-    public ApiResponse<ApiResponse.CustomBody<Void>> validateMemberEmailCodeForSignUp(@RequestBody @Valid MemberEmailCodeRequestDTO memberEmailCodeRequestDTO){
-        memberEmailService.validateCodeForSignUp(memberEmailCodeRequestDTO.getEmailVerificationCode(),memberEmailCodeRequestDTO.getEmail());
-        return ApiResponseGenerator.success(HttpStatus.OK);
-    }
-
-    @GetMapping("/member/code")
-    public ApiResponse<ApiResponse.CustomBody<MemberEmailCodeResponseDTO>> sendEmail(@Valid MemberEmailRequestDTO memberEmailRequestDTO) throws MessagingException {
-        MemberEmailCodeResponseDTO memberEmailCodeResponseDTO = memberEmailService.sendEmail(memberEmailRequestDTO);
-        return ApiResponseGenerator.success(memberEmailCodeResponseDTO,HttpStatus.OK);
-    }
-
+    @ApiOperation(value = "회원가입시 인증 코드 발송")
     @GetMapping("/member/signup/code")
     public ApiResponse<ApiResponse.CustomBody<MemberEmailCodeResponseDTO>> sendEmailForSignUp(@Valid MemberEmailRequestDTO memberEmailRequestDTO) throws MessagingException {
         MemberEmailCodeResponseDTO memberEmailCodeResponseDTO = memberEmailService.sendEmailForSignup(memberEmailRequestDTO);
         return ApiResponseGenerator.success(memberEmailCodeResponseDTO,HttpStatus.OK);
     }
 
+    @ApiOperation(value = "회원가입시 인증 코드를 발송한 후에, 인증할 때 사용")
+    @PostMapping("/member/signup/valid-code")
+    public ApiResponse<ApiResponse.CustomBody<Void>> validateMemberEmailCodeForSignUp(@RequestBody @Valid MemberEmailCodeRequestDTO memberEmailCodeRequestDTO){
+        memberEmailService.validateCodeForSignUp(memberEmailCodeRequestDTO.getEmailVerificationCode(),memberEmailCodeRequestDTO.getEmail());
+        return ApiResponseGenerator.success(HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "아이디 찾기 시, 해당 이메일에 인증코드 발송할 때 사용")
+    @GetMapping("/member/code")
+    public ApiResponse<ApiResponse.CustomBody<MemberEmailCodeResponseDTO>> sendEmail(@Valid MemberEmailRequestDTO memberEmailRequestDTO) throws MessagingException {
+        MemberEmailCodeResponseDTO memberEmailCodeResponseDTO = memberEmailService.sendEmail(memberEmailRequestDTO);
+        return ApiResponseGenerator.success(memberEmailCodeResponseDTO,HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "아이디 찾기 시, 인증코드를 보낸 후에 인증하기 버튼을 누를 때 사용")
+    @PostMapping("/member/valid-code")
+    public ApiResponse<ApiResponse.CustomBody<Void>> validateMemberEmailCode(@RequestBody @Valid MemberEmailCodeRequestDTO memberEmailCodeRequestDTO){
+        memberEmailService.validateCode(memberEmailCodeRequestDTO.getEmailVerificationCode(),memberEmailCodeRequestDTO.getEmail());
+        return ApiResponseGenerator.success(HttpStatus.OK);
+    }
 
 
+    @ApiOperation(value = "아이디를 찾을 때 사용")
     @GetMapping("/member/loginId")
     public ApiResponse<ApiResponse.CustomBody<String>> findLoginId(@ModelAttribute @Valid EmailCodeRequestDTO emailCodeRequestDTO) {
         return ApiResponseGenerator.success(findMemberAccountHandler.executeForLoginId(emailCodeRequestDTO),HttpStatus.OK);
     }
 
+    @ApiOperation(value = "비밀번호를 찾을 때 사용")
     @GetMapping("/member/password")
     public ApiResponse<ApiResponse.CustomBody<Void>> findPassword(@ModelAttribute @Valid MemberPasswordRequestDTO memberPasswordRequestDTO) throws MessagingException {
         findMemberAccountHandler.executeForPassword(memberPasswordRequestDTO);
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
 
+
+    @ApiOperation(value = "아이디를 수정할 때 사용")
     @PatchMapping("/member/loginId")
     public ApiResponse<ApiResponse.CustomBody<Void>> updateLoginId(@RequestBody UpdateMemberLoginIdRequestDTO updateMemberLoginIdRequestDTO){
         memberAccountService.updateLoginId(updateMemberLoginIdRequestDTO);
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
 
+    @ApiOperation(value = "비밀번호를 찾을 때 사용")
     @PatchMapping("/member/password")
     public ApiResponse<ApiResponse.CustomBody<Void>> updatePassword(@RequestBody UpdateMemberPasswordRequestDTO updateMemberPasswordRequestDTO){
         memberAccountService.updatePassword(updateMemberPasswordRequestDTO);
--- src/main/java/com/theZ/dotoring/app/menti/controller/MentiController.java
+++ src/main/java/com/theZ/dotoring/app/menti/controller/MentiController.java
@@ -7,6 +7,7 @@ import com.theZ.dotoring.app.menti.handler.UpdateMentiDesiredFieldHandler;
 import com.theZ.dotoring.app.menti.service.MentiService;
 import com.theZ.dotoring.common.ApiResponse;
 import com.theZ.dotoring.common.ApiResponseGenerator;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -30,24 +31,28 @@ public class MentiController {
     private final FindAllMentiHandler findAllMentiHandler;
     private final UpdateMentiDesiredFieldHandler updateMentiDesiredFieldHandler;
 
+    @ApiOperation(value = "멘티 최종 회원가입때 사용")
     @PostMapping("/signup-menti")
     public ApiResponse<ApiResponse.CustomBody<Void>> saveMenti(@RequestPart List<MultipartFile> certifications , @RequestPart @Valid SaveMentiRqDTO saveMentiRqDTO) throws IOException {
         saveMentiHandler.execute(saveMentiRqDTO,certifications);
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
 
+    @ApiOperation(value = "멘티 회원가입 시 닉네임 중복 검사할 때 사용", notes = "이름은 3자 이상 8자 이하로 입력해주세요.")
     @PostMapping("/menti/valid-nickname")
     public ApiResponse<ApiResponse.CustomBody<Void>> validateMentiNickname(@RequestBody @Valid ValidateMentiNicknameRqDTO validateMentiNicknameRqDTO){
         mentiService.validateNickname(validateMentiNicknameRqDTO);
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
 
+    @ApiOperation(value = "멘티 홈에서 해당 멘티 상세 조회시 사용")
     @GetMapping("/menti/{id}")
     public ApiResponse<ApiResponse.CustomBody<FindMentiByIdRespDTO>> findMentiById(@PathVariable Long id){
         FindMentiByIdRespDTO findMentiByIdRespDTO = mentiService.findMentiWithProfile(id);
         return ApiResponseGenerator.success(findMentiByIdRespDTO,HttpStatus.OK);
     }
 
+    @ApiOperation(value = "멘티 홈에서 도토링 추천 방식에따른 멘티들 추천")
     @GetMapping("/menti")
     public ApiResponse<ApiResponse.CustomBody<Slice<FindAllMentiRespDTO>>> findAllMentiBySlice(
             @RequestParam(required = false) Long lastMentiId, @RequestParam(defaultValue = "10") Integer size, Long mentoId){
@@ -67,19 +72,21 @@ public class MentiController {
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
 
+    @ApiOperation(value = "마이페이지에서 멘티 선호 멘토링 수정", notes = "선호 멘토링에 대해서 10글자 이상 300자 이하를 작성해야합니다.")
     @PatchMapping("/menti/preferredMentoring")
     public ApiResponse<ApiResponse.CustomBody<FindAllMentiRespDTO>> updateMentiMentoringSystem(@RequestBody @Valid UpdateMentiMentoringSystemRqDTO updateMentiMentoringSystemRqDTO){
         FindAllMentiRespDTO findAllMentiRespDTO = mentiService.updatePreferredMentoring(updateMentiMentoringSystemRqDTO);
         return ApiResponseGenerator.success(findAllMentiRespDTO,HttpStatus.OK);
     }
 
+    @ApiOperation(value = "마이페이지에서 멘티 소개 수정 ", notes = "10글자에서 100글자 사이로 입력해주세요")
     @PatchMapping("/menti/introduction")
     public ApiResponse<ApiResponse.CustomBody<FindMentiByIdRespDTO>> updateMentiIntroduction(@RequestBody @Valid UpdateMentiIntroductionRqDTO updateMentiIntroductionRqDTO){
         FindMentiByIdRespDTO findMentiByIdRespDTO = mentiService.updateItroduction(updateMentiIntroductionRqDTO);
         return ApiResponseGenerator.success(findMentiByIdRespDTO,HttpStatus.OK);
     }
 
-    // 닉네임 수정
+    @ApiOperation(value = "마이페이지에서 멘티 닉네임 수정 ", notes = "이름은 3자 이상 8자 이하로 입력해주세요.")
     @PatchMapping("/menti/nickname")
     public ApiResponse<ApiResponse.CustomBody<FindMentiByIdRespDTO>> updateMentiNickname(@RequestBody @Valid UpdateMentiNicknameRqDTO updateMentiNicknameRqDTO){
         FindMentiByIdRespDTO findMentiByIdRespDTO = mentiService.updateNickname(updateMentiNicknameRqDTO);
@@ -87,7 +94,7 @@ public class MentiController {
     }
 
 
-    // 희망 멘토링 분야
+    @ApiOperation(value = "마이페이지에서 희망 멘토링 분야 수정 ")
     @PatchMapping("/menti/desiredField")
     public ApiResponse<ApiResponse.CustomBody<FindMentiByIdRespDTO>> updateMentiDesiredField(@RequestBody @Valid UpdateMentiDesiredFieldRqDTO updateMentiDesiredFieldRqDTO){
         FindMentiByIdRespDTO findMentiByIdRespDTO = updateMentiDesiredFieldHandler.execute(updateMentiDesiredFieldRqDTO);
