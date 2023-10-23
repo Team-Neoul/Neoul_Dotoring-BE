6757cbd5f3c308e6ea122ad8965b5676e98b2bd7
Sonny <hds650588@gmail.com>
2023-10-23 23:32:07 +0900

    refactor : SaveMentoRqDTO 변경으로 인한 수정

--- src/main/java/com/theZ/dotoring/app/mento/controller/MentoController.java
+++ src/main/java/com/theZ/dotoring/app/mento/controller/MentoController.java
@@ -15,12 +15,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
 import javax.validation.Valid;
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,9 +31,9 @@ public class MentoController {
     private final UpdateMentoDesiredFieldHandler updateMentoDesiredFieldHandler;
 
     @ApiOperation(value = "멘토 최종 회원가입때 사용")
-    @PostMapping("/signup-mento")
-    public ApiResponse<ApiResponse.CustomBody<Void>> saveMento(@RequestPart List<MultipartFile> certifications , @RequestPart @Valid SaveMentoRqDTO saveMentoRqDTO) throws IOException {
-        saveMentoHandler.execute(saveMentoRqDTO,certifications);
+    @PostMapping(value = "/signup-mento", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ApiResponse<ApiResponse.CustomBody<Void>> saveMento(@ModelAttribute @Valid SaveMentoRqDTO saveMentoRqDTO) throws IOException {
+        saveMentoHandler.execute(saveMentoRqDTO);
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
 
