e9013e70b759299f5cf92f4b07bfd3b0e8784f19
Sonny <hds650588@gmail.com>
2023-10-23 18:56:31 +0900

    refactor : Swagger 이슈로 인해 @RequestPart -> @ModelAttribute로 변경

--- src/main/java/com/theZ/dotoring/app/menti/controller/MentiController.java
+++ src/main/java/com/theZ/dotoring/app/menti/controller/MentiController.java
@@ -14,12 +14,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
 import javax.validation.Valid;
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,9 +30,9 @@ public class MentiController {
     private final UpdateMentiDesiredFieldHandler updateMentiDesiredFieldHandler;
 
     @ApiOperation(value = "멘티 최종 회원가입때 사용")
-    @PostMapping("/signup-menti")
-    public ApiResponse<ApiResponse.CustomBody<Void>> saveMenti(@RequestPart List<MultipartFile> certifications , @RequestPart @Valid SaveMentiRqDTO saveMentiRqDTO) throws IOException {
-        saveMentiHandler.execute(saveMentiRqDTO,certifications);
+    @PostMapping(value = "/signup-menti", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ApiResponse<ApiResponse.CustomBody<Void>> saveMenti(@ModelAttribute @Valid SaveMentiRqDTO saveMentiRqDTO) throws IOException {
+        saveMentiHandler.execute(saveMentiRqDTO);
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
 
