76b2ab1947e687342ab749dc99ba83232f8db5ca
Sonny <hds650588@gmail.com>
2023-10-25 21:36:51 +0900

    feat : 특정 요청에 대한 인증/인가 규칙 설정

--- src/main/java/com/theZ/dotoring/config/WebSecurityConfig.java
+++ src/main/java/com/theZ/dotoring/config/WebSecurityConfig.java
@@ -23,6 +23,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final DotoringLoginSuccessHandler dotoringLoginSuccessHandler;
     private final MemberDetailService memberDetailService;
+
+    private static final String [] PERMIT_PATH_PATTERN = {
+            "/api/fields","/api/majors","/api/member/code","/api/member/loginId","/api/member/password",
+            "/api/member/signup/code","/api/member/signup/valid-code","/api/member/valid-code","/api/member/valid-loginId",
+            "/api/member/login","/api/menti/valid-nickname","/api/signup-menti","/api/mento/valid-nickname","/api/signup-mento"
+    };
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.exceptionHandling().accessDeniedHandler((request, response, authException) -> {
+            FilterResponseUtils.unAuthorized(response);
+        });
+        http.addFilterBefore(dotoringAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable()
+                // 인증 정보를 세션에 저장하지 않고, 상태가 없는(무상태) 웹 애플리케이션을 구성하기 위해 사용됩니다.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+
+                .authorizeRequests()//요청에 대한 인증/인가 규칙을 설정합니다. 즉, 특정한 URL 패턴에 대해 어떤 역할(Role)을 가진 사용자만 접근을 허용할지를 지정합니다.
+                        .antMatchers("/api/menti","/api/menti/{id}","/api/mento/desiredField","/api/mento/introduction","/api/mento/mentoringSystem","/api/mento/nickname","/api/profile").hasRole("MENTO")
+                        .antMatchers("/api/mento","/api/mento/{id}","/api/menti/desiredField","/api/menti/introduction","/api/menti/mentoringSystem","/api/menti/nickname","/api/profile").hasRole("MENTI")
+                        .antMatchers(PERMIT_PATH_PATTERN).permitAll(); // PERMIT_PATH_PATTERN에 해당하는 URL은 모든 사용자에게 접근이 허용됩니다. 즉, 인증된 사용자든 아니든 상관없이 모두 접근할 수 있습니다.
+        http.headers().frameOptions().disable();
+        http.authorizeRequests().antMatchers("/h2-console/*").permitAll();
+
+        //http.addFilterBefore(jwtAuthenticationFilter(), BasicAuthenticationFilter.class);
+
+    }
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
