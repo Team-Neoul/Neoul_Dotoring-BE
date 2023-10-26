b4681c00f5743d205251dba0bfe6ab5ceb0f8cd0
Sonny <hds650588@gmail.com>
2023-10-26 19:18:24 +0900

    refactor : Optional을 사용하여 NullPointerException 방지

--- src/main/java/com/theZ/dotoring/app/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/theZ/dotoring/app/auth/JwtAuthenticationFilter.java
@@ -14,6 +14,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 
 @Slf4j
@@ -28,28 +29,34 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
 
-        String accessToken = request.getHeader(AuthConstants.AUTH_HEADER);
+        Optional<String> accessToken = Optional.ofNullable(request.getHeader(AuthConstants.AUTH_HEADER));
+        log.debug("accessToken, {}", accessToken.get());
 
-        if(accessToken == null){
+        if(accessToken.isEmpty()){
             chain.doFilter(request,response);
             return;
         }
 
-        boolean isValidAccessToken = Token.isValidAccessToken(accessToken);
+        boolean isValidAccessToken = Token.isValidAccessToken(accessToken.get(),response);
 
         if(isValidAccessToken){
-            Authentication authentication = memberDetailService.getAuthentication(accessToken);
+            log.debug("isValidAccessToken, {}", true);
+            Authentication authentication = memberDetailService.getAuthentication(accessToken.get());
             SecurityContextHolder.getContext().setAuthentication(authentication);
             chain.doFilter(request,response);
         }else{
-            String refreshToken = getRefreshToken(request.getCookies());
+            log.debug("isValidAccessToken, {}", false);
+            Optional<Cookie[]> cookies = Optional.ofNullable(request.getCookies());
+            Optional<String> refreshToken = Optional.ofNullable(getRefreshToken(cookies));
             if(refreshToken.isEmpty()){
                 response.sendRedirect(request.getContextPath() + "/api/auth/reRequest");
             }else{
-                boolean isValidRefreshToken = Token.isValidRefreshToken(refreshToken);
+                boolean isValidRefreshToken = Token.isValidRefreshToken(refreshToken.get(),response);
                 if(isValidRefreshToken){
+                    log.debug("isValidRefreshToken, {}", true);
                     chain.doFilter(request,response);
                 }else {
+                    log.debug("isValidRefreshToken, {}", false);
                     response.sendRedirect(request.getContextPath() + "/api/auth/fail");
                 }
             }
@@ -58,8 +65,13 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
     }
 
-    private String getRefreshToken(Cookie[] cookies){
-        for(Cookie cookie : cookies) {
+    private String getRefreshToken(Optional<Cookie[]> cookies){
+
+        if(cookies.isEmpty()){
+            return null;
+        }
+
+        for(Cookie cookie : cookies.get()) {
             if(cookie.getName().equals("refreshToken")) {
                 return cookie.getValue();
             }
