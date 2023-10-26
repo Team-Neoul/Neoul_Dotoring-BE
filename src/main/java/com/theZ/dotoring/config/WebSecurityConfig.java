553d9820892515a93becfbf880fac01a3b16eb8b
Sonny <hds650588@gmail.com>
2023-10-26 10:05:56 +0900

    refactor : /api/mento/valid-nickname이 인식이 안되는 오류를 antMatchers permitAll()을 위치를  올려서 해결

--- src/main/java/com/theZ/dotoring/config/WebSecurityConfig.java
+++ src/main/java/com/theZ/dotoring/config/WebSecurityConfig.java
@@ -24,11 +24,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final DotoringLoginSuccessHandler dotoringLoginSuccessHandler;
     private final MemberDetailService memberDetailService;
 
-    private static final String [] PERMIT_PATH_PATTERN = {
-            "/api/fields","/api/majors","/api/member/code","/api/member/loginId","/api/member/password",
-            "/api/member/signup/code","/api/member/signup/valid-code","/api/member/valid-code","/api/member/valid-loginId",
-            "/api/member/login","/api/menti/valid-nickname","/api/signup-menti","/api/mento/valid-nickname","/api/signup-mento"
-    };
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -43,9 +38,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().disable()
 
                 .authorizeRequests()//요청에 대한 인증/인가 규칙을 설정합니다. 즉, 특정한 URL 패턴에 대해 어떤 역할(Role)을 가진 사용자만 접근을 허용할지를 지정합니다.
-                        .antMatchers("/api/menti","/api/menti/{id}","/api/mento/desiredField","/api/mento/introduction","/api/mento/mentoringSystem","/api/mento/nickname","/api/profile").hasRole("MENTO")
-                        .antMatchers("/api/mento","/api/mento/{id}","/api/menti/desiredField","/api/menti/introduction","/api/menti/mentoringSystem","/api/menti/nickname","/api/profile").hasRole("MENTI")
-                        .antMatchers(PERMIT_PATH_PATTERN).permitAll(); // PERMIT_PATH_PATTERN에 해당하는 URL은 모든 사용자에게 접근이 허용됩니다. 즉, 인증된 사용자든 아니든 상관없이 모두 접근할 수 있습니다.
+                    .antMatchers("/api/fields","/api/majors","/api/member/code","/api/member/loginId","/api/member/password",
+                            "/api/member/signup/code","/api/member/signup/valid-code","/api/member/valid-code","/api/member/valid-loginId",
+                            "/api/menti/valid-nickname","/api/mento/valid-nickname","/api/member/login","/api/signup-menti","/api/signup-mento").permitAll()
+                    .antMatchers("/api/menti","/api/menti/{id}","/api/mento/desiredField","/api/mento/introduction","/api/mento/mentoringSystem","/api/mento/nickname","/api/profile").hasRole("MENTO")
+                    .antMatchers("/api/mento","/api/mento/{id}","/api/menti/desiredField","/api/menti/introduction","/api/menti/mentoringSystem","/api/menti/nickname","/api/profile").hasRole("MENTI");
+
         http.headers().frameOptions().disable();
         http.authorizeRequests().antMatchers("/h2-console/*").permitAll();
 
