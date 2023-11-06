66dfa2c10faded85c99e4c3ea408d170eae69caa
Sonny <hds650588@gmail.com>
2023-11-06 12:17:51 +0900

    feat : resourceHanler의 경로 변경 및 이미지 캐싱 시간 설정

--- src/main/java/com/theZ/dotoring/config/WebConfig.java
+++ src/main/java/com/theZ/dotoring/config/WebConfig.java
@@ -1,22 +1,29 @@
 package com.theZ.dotoring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${file.path}")
+    private String filePath;
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/static/", "classpath:/public/", "classpath:/",
             "classpath:/resources/", "classpath:/META-INF/resources/", "classpath:/META-INF/resources/webjars/" };
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                        .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+        WebMvcConfigurer.super.addResourceHandlers(registry);
 
-        registry.addResourceHandler("/image/**")
-                .addResourceLocations("file:src/main/resources/image/");
+        registry
+                .addResourceHandler("/images/**")
+                .addResourceLocations("file:" + filePath)
+                .setCachePeriod(60 * 60)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
     }
 
 
