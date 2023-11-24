7056a249caf82f45dfcae1eb4f1ebe9fbc83c953
DEVdongbaek <yardyard@likelion.org>
2023-11-24 15:29:20 +0900

    fix: FcmConfig 주석처리

--- src/main/java/com/theZ/dotoring/config/FCMConfig.java
+++ src/main/java/com/theZ/dotoring/config/FCMConfig.java
@@ -14,29 +14,30 @@ import java.util.List;
 
 @Configuration
 public class FCMConfig {
-    @Bean
-    FirebaseMessaging firebaseMessaging() throws IOException {
-        ClassPathResource resource = new ClassPathResource("firebase/dotoring-bec2b-firebase-adminsdk-kklf4-7a5220e90b.json");
-
-        InputStream refreshToken = resource.getInputStream();
-
-        FirebaseApp firebaseApp = null;
-        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
-
-        if (firebaseAppList != null && !firebaseAppList.isEmpty()) {
-            for (FirebaseApp app : firebaseAppList) {
-                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
-                    firebaseApp = app;
-                }
-            }
-        } else {
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
-                    .build();
-
-            firebaseApp = FirebaseApp.initializeApp(options);
-        }
-
-        return FirebaseMessaging.getInstance(firebaseApp);
-    }
+    // todo
+//    @Bean
+//    FirebaseMessaging firebaseMessaging() throws IOException {
+//        ClassPathResource resource = new ClassPathResource("firebase/dotoring-bec2b-firebase-adminsdk-kklf4-7a5220e90b.json");
+//
+//        InputStream refreshToken = resource.getInputStream();
+//
+//        FirebaseApp firebaseApp = null;
+//        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+//
+//        if (firebaseAppList != null && !firebaseAppList.isEmpty()) {
+//            for (FirebaseApp app : firebaseAppList) {
+//                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+//                    firebaseApp = app;
+//                }
+//            }
+//        } else {
+//            FirebaseOptions options = FirebaseOptions.builder()
+//                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
+//                    .build();
+//
+//            firebaseApp = FirebaseApp.initializeApp(options);
+//        }
+//
+//        return FirebaseMessaging.getInstance(firebaseApp);
+//    }
 }
