fc3ff61b19e2436aa7c1a7383732a0a2a9536c46
Sonny <hds650588@gmail.com>
2023-12-01 00:38:38 +0900

    feat : 멘토링 방식 수정용 DTO 추가

--- src/main/java/com/theZ/dotoring/app/mento/dto/UpdateMentoringSystemRqDTO.java
+++ src/main/java/com/theZ/dotoring/app/mento/dto/UpdateMentoringSystemRqDTO.java
@@ -7,8 +7,7 @@ import javax.validation.constraints.Size;
 @Data
 public class UpdateMentoringSystemRqDTO {
 
-    private Long mentoId;
-
     @Size(min = 10, max = 300, message = "멘토링 방식은 10글자 이상 300자 이하를 작성해야합니다.")
     private String mentoringSystem;
+
 }
