dbffec48a426ab48d9e94bf26f58b979c6ab7966
Sonny <hds650588@gmail.com>
2023-12-01 00:37:32 +0900

    feat : 선호 멘토링 변경 DTO 추가

---
+++ src/main/java/com/theZ/dotoring/app/menti/dto/UpdatePreferredMentoringRqDTO.java
@@ -0,0 +1,13 @@
+package com.theZ.dotoring.app.menti.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePreferredMentoringRqDTO {
+
+    private String preferredMentoringSystem;
+}
